3a6a398e9df874e1281f3eb2fe2f1730c2fe4062
Michael-lima <123214915+Michael-lima@users.noreply.github.com>
2023-02-02 12:17:12 -0300

    Código freeway

---
+++ images.js
@@ -0,0 +1,30 @@
+
+
+
+
+function setup() {
+  createCanvas(500, 400);
+  somDaTrilha.loop();
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  image( imagemDoAtor, xAtor ,yAtor ,30 ,30);
+ 
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialCarros();
+  mostraCarro();
+  colisaoAtor();
+  pontos();
+   marcaPontos();
+} 
+
+function mostraCarro(){
+  for (var i = 0;  i < imagemCarros.length;  i++ ){
+  
+  image(imagemCarros[i], xCarros[i] ,yCarros[i] ,40 ,30);
+ 
+  }
+
+}
